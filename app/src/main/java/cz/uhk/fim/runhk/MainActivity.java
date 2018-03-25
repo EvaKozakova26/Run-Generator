7043756a428a84509d2c60655f537dbb6c17474f
bezdyjoe <kozakev26@gmail.com>
2018-03-25 21:43:16 +0200

    first commit

---
+++ app/src/main/java/cz/uhk/fim/runhk/MainActivity.java
@@ -0,0 +1,28 @@
+package cz.uhk.fim.runhk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnRun;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnRun = findViewById(R.id.btnRun);
+        btnRun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
