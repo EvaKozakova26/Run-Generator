9fc92e810fb5b52b433c1d205c220f0733b3f0ae
EvaKozakova26 <33025572+evakozakova26@users.noreply.github.com>
2018-04-02 20:01:44 +0200

    added Player entity

---
+++ app/src/main/java/cz/uhk/fim/runhk/model/Player.java
@@ -0,0 +1,63 @@
+package cz.uhk.fim.runhk.model;
+
+/**
+ * Created by EvaKozakova on 02.04.2018.
+ */
+
+public class Player {
+
+    private String nickname;
+    private String email;
+    private String password;
+
+    private int level;
+    private int exps;
+
+    public Player(String nickname, String email, String password, int level, int exps) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.level = level;
+        this.exps = exps;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getExps() {
+        return exps;
+    }
+
+    public void setExps(int exps) {
+        this.exps = exps;
+    }
+}
