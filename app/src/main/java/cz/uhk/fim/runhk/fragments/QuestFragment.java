cd5cd478ae6ac5d7088d480ec1a46dd22aad4d6f
EvaKozakova26 <evkozakova@seznam.cz>
2018-06-05 21:13:36 +0200

    added change location settings

--- app/src/main/java/cz/uhk/fim/runhk/fragments/QuestFragment.java
+++ app/src/main/java/cz/uhk/fim/runhk/fragments/QuestFragment.java
@@ -3,21 +3,31 @@ package cz.uhk.fim.runhk.fragments;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.IntentSender;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import cz.uhk.fim.runhk.R;
 
 /**
@@ -28,6 +38,8 @@ public class QuestFragment extends Fragment implements View.OnClickListener {
     OnButtonClickedInterface onButtonClickedInterface;
 
     FusedLocationProviderClient fusedLocationProviderClient;
+
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
 
     double lat;
     double lon;
@@ -54,8 +66,46 @@ public class QuestFragment extends Fragment implements View.OnClickListener {
 
         getLastKnownLocation();
 
+        LocationRequest mLocationRequestHighAccuracy = new LocationRequest();
+        mLocationRequestHighAccuracy.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+        // location updates
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        SettingsClient client = LocationServices.getSettingsClient(getContext());
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(getActivity(), new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+            }
+        });
+
+        task.addOnFailureListener(getActivity(), new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(getActivity(),
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+
         return view;
     }
+
 
     public void setOnButtonClickedInterface(OnButtonClickedInterface onButtonClickedInterface1) {
         onButtonClickedInterface = onButtonClickedInterface1;
@@ -96,11 +146,6 @@ public class QuestFragment extends Fragment implements View.OnClickListener {
     @SuppressLint("StaticFieldLeak")
     private void getLastKnownLocation() {
         new AsyncTask<Void, Void, List<Double>>() {
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-            }
 
             @SuppressLint("MissingPermission")
             @Override
