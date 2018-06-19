b96b9a182d95e8892bc02f32beef38f1110064ca
EvaKozakova26 <evkozakova@seznam.cz>
2018-06-19 19:40:49 +0200

    added profile pictures in PlayerEditInfoActivity (read only)

--- app/src/main/java/cz/uhk/fim/runhk/activities/PlayerEditInfoActivity.java
+++ app/src/main/java/cz/uhk/fim/runhk/activities/PlayerEditInfoActivity.java
@@ -1,16 +1,24 @@
 package cz.uhk.fim.runhk.activities;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import cz.uhk.fim.runhk.R;
 
@@ -20,10 +28,25 @@ public class PlayerEditInfoActivity extends AppCompatActivity {
     FirebaseUser currentUser;
     DatabaseReference databaseReference;
 
+    ImageView imgFemale;
+    ImageView imgMale;
+
+    StorageReference femaleRefImg;
+    StorageReference maleRefImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_edit_info);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        femaleRefImg = storage.getReference().child("images/female.png");
+        maleRefImg = storage.getReference().child("images/male.png");
+        imgFemale = findViewById(R.id.imgViewFemale);
+        imgMale = findViewById(R.id.imgViewMale);
+
+        createProfilePictures();
 
         final EditText editTextNickname = findViewById(R.id.editTextNickname);
 
@@ -41,6 +64,33 @@ public class PlayerEditInfoActivity extends AppCompatActivity {
                 Intent intent = new Intent(PlayerEditInfoActivity.this, PlayerProfileActivity.class);
                 finish();
                 startActivity(intent);
+            }
+        });
+
+    }
+
+    private void createProfilePictures() {
+        femaleRefImg.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(imgFemale);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+        maleRefImg.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(imgMale);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //
             }
         });
     }
